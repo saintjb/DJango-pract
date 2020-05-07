61dd3dd7ceb8bac34138e7f344b3fd684779164c
saintjb <48727494+saintjb@users.noreply.github.com>
2020-05-07 13:21:37 +0300

    'changes'

--- newapp/views.py
+++ newapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import render, redirect
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -18,3 +19,49 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'newapp/topic.html', context)
 
+
+def new_topic(request):
+    """Define new topic"""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('newapp:topics')
+
+    context = {'form': form}
+    return render(request, 'newapp/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Define new entry"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('newapp:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form':form}
+    return render(request, 'newapp/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit entries"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('newapp:topic', topic_id = topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'newapp/edit_entry.html', context)
